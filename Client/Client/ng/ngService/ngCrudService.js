e8ac3e89b99ef81fae6eb08a65985b32c72b466d
saipramodbalantrapu <saipramod.balatrapu@dinoosys.com>
2018-03-12 14:29:54 +0530

    Client Solution

---
+++ Client/Client/ng/ngService/ngCrudService.js
@@ -0,0 +1,39 @@
+﻿app.service('CrudService', function ($http) {
+
+    var urlGet = '';
+    this.post = function (apiRoute, Model) {
+        debugger;
+        var request = $http({
+            method: "post",
+            url: apiRoute,
+            data: Model
+        });
+        return request;
+    }
+    this.put = function (apiRoute, Model) {
+        var request = $http({
+            method: "put",
+            url: apiRoute,
+            data: Model
+        });
+        return request;
+    }
+    this.delete = function (apiRoute) {
+        var request = $http({
+            method: "delete",
+            url: apiRoute
+        });
+        return request;
+    }
+    this.getAll = function (apiRoute) {
+
+        urlGet = apiRoute;
+        return $http.get(urlGet);
+    }
+
+    this.getbyID = function (apiRoute, ID) {
+
+        urlGet = apiRoute + '/' + ID;
+        return $http.get(urlGet);
+    }
+});
