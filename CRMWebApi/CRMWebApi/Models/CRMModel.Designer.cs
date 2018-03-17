2dad9c80f8286651306e546bb0df66a432a9c09e
saipramodbalantrapu <saipramod.balatrapu@dinoosys.com>
2018-03-17 17:27:53 +0530

    Added PK for Bank and Address Model

--- CRMWebApi/CRMWebApi/Models/CRMModel.Designer.cs
+++ CRMWebApi/CRMWebApi/Models/CRMModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\CRM\crf_dev\CRMWebApi\CRMWebApi\Models\CRMModel.edmx'. 
+﻿// T4 code generation is enabled for model 'E:\CRM Project\CRMWebApi\CRMWebApi\Models\CRMModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
