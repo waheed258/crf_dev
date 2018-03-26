64532d275110e5fb4caed878ee41b591151109a5
saipramodbalantrapu <saipramod.balatrapu@dinoosys.com>
2018-03-26 11:33:21 +0530

    Added Login Page

---
+++ CRM/CRMAdminProject/AdminForms/Login.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using EntityManager;
+using BusinessLogic;
+using System.Data;
+
+public partial class AdminForms_Login : System.Web.UI.Page
+{
+    EncryptDecrypt encryptdecrypt = new EncryptDecrypt();
+    LoginBL loginBL = new LoginBL();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            if (!IsPostBack)
+            {
+                Session["LoginId"] = null;
+                Session["Password"] = null;
+            }
+        }
+        catch { }
+    }
+    protected void btnLogin_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            if (txtUserName.Text == "admin" && txtPassword.Text == "admin")
+            {
+                Response.Redirect("NewAdvisor.aspx");
+            }
+            else
+            {
+                DataSet ds = loginBL.ValidateUser(txtUserName.Text);
+                string passowrd = string.Empty;
+                if (ds.Tables[0].Rows.Count > 0)
+                {
+                    passowrd = ds.Tables[0].Rows[0]["Password"].ToString();
+                    // passowrd = encryptdecrypt.Decrypt(ds.Tables[0].Rows[0]["Password"].ToString());
+                    if (passowrd == txtPassword.Text)
+                    {
+                        Session["Name"] = ds.Tables[0].Rows[0]["FirstName"].ToString() + " " + ds.Tables[0].Rows[0]["LastName"].ToString();
+                        Session["AdvisorType"] = ds.Tables[0].Rows[0]["AdvisorType"].ToString();
+                        Session["LoginId"] = ds.Tables[0].Rows[0]["LoginId"].ToString();
+                        Response.Redirect("NewAdvisor.aspx", false);
+                    }
+                    else
+                    {
+                        lblError.Text = "Password is incorrect!";
+                    }
+                }
+                else
+                {
+                    lblError.Text = "User Name is incorrect!";
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            lblError.Text = "Please check credentials";
+        }
+    }
+}
---
+++ CRM/CRMAdminProject/App_Code/BusinessLogic/LoginBL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DataManager;
+using EntityManager;
+using System.Data;
+using System.Collections;
+/// <summary>
+/// Summary description for LoginBL
+/// </summary>
+public class LoginBL
+{
+    DataUtilities dataUtilities = new DataUtilities();
+    DataSet ds = new DataSet();
+    public DataSet ValidateUser(string EmailId)
+    {
+        Hashtable hashtable = new Hashtable();
+        hashtable.Add("@EmailID", EmailId);
+        ds = dataUtilities.ExecuteDataSet("ValidateUser", hashtable);
+        return ds;
+    }
+}
