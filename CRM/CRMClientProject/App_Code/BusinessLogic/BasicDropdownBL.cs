960aeb4b6e5bb819eb2c7ee847b0b6cf6eab63ce
saikishoreraju <saikishore.addada@dinoosys.com>
2018-03-27 14:39:48 +0530

    Adding City,country,State,Accounttpe dropdown lists

---
+++ CRM/CRMClientProject/App_Code/BusinessLogic/BasicDropdownBL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using EntityManager;
+using System.Collections;
+using DataManager;
+
+
+namespace BusinessLogic
+{
+    public class ClientProfileBL
+    {
+        DataUtilities dataUtilities = new DataUtilities();
+        DataSet ds = new DataSet();
+
+        public DataSet GetCountry()
+        {
+            ds = dataUtilities.ExecuteDataSet("GetCountry");
+            return ds;
+        }
+
+        public DataSet GetProvince()
+        {
+            ds = dataUtilities.ExecuteDataSet("GetProvince");
+            return ds;
+        }
+
+        public DataSet GetCity()
+        {
+            ds = dataUtilities.ExecuteDataSet("GetCity");
+            return ds;
+        }
+        public DataSet GetAccountType()
+        {
+            ds = dataUtilities.ExecuteDataSet("GetAccountType");
+            return ds;
+        }
+
+    }
+}
