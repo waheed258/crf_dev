4a6ec501e941d14ccd2105f7b463de07b008e9c6
surekhatelidevara <surekha.telidevara@dinoosys.com>
2018-04-02 18:22:56 +0530

    Reset Password form
    Added Reset form and modified LayoutMaster and Added ResetPasswordBL

---
+++ CRM/CRMClientProject/App_Code/BusinessLogic/ResetPasswordBL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EntityManager;
+using System.Collections;
+using DataManager;
+using System.Data;
+
+/// <summary>
+/// Summary description for ResetPasswordBL
+/// </summary>
+namespace BusinessLogic
+{
+    public class ResetPasswordBL
+    {
+        DataUtilities dataUtilities = new DataUtilities();
+        public int ResetPassword(string OldPassword,string NewPassword,string SAID)
+        {
+            Hashtable hashtable = new Hashtable();
+            hashtable.Add("@OldPassword", OldPassword);
+            hashtable.Add("@NewPassword", NewPassword);
+            hashtable.Add("@SAID", SAID);
+            int result = dataUtilities.ExecuteNonQuery("ResetPassword", hashtable);
+            return result;
+        }
+    }
+}
